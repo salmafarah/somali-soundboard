fa83d07b36455447316073d74e78da8004bc04bd
Salma Farah <salmafarah08@gmail.com>
2020-06-19 21:02:00 -0400

    done

---
+++ main.js
@@ -0,0 +1,29 @@
+let audio = new Audio();
+audio.src = "hard cry.mp3"; 
+
+let audio1 = new Audio()
+audio1.src = "softcry.mp3";
+
+let audio2 = new Audio()
+audio2.src = "water.mp3";
+
+let audio3 = new Audio()
+audio3.src = "speed walking.mp3";
+
+let audio4 = new Audio()
+audio4.src = "fastwalking.mp3";
+
+let audio5 = new Audio()
+audio5.src = "crazy.mp3";
+
+let audio6 = new Audio()
+audio6.src = "eww.mp3";
+
+let audio7 = new Audio()
+audio7.src = "mashallah.mp3";
+
+let audio8 = new Audio()
+audio8.src = "wow.mp3";
+
+let audio9 = new Audio()
+audio9.src = "laughing.mp3";
